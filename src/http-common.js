2c13d49f452db5400cd61756e325ce6d1d4a1972
cicerorod <cicerorod@gmail.com>
2020-07-05 03:18:52 -0300

    alteraçã de endereço

--- src/http-common.js
+++ src/http-common.js
@@ -1,9 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:8081/',
+  //baseURL: "http://localhost:3001",
+  baseURL: "https://scenic-big-bend-74242.herokuapp.com",
   headers: {
-    'Content-type': 'application/json',
+    "Content-type": "application/json",
   },
 });
